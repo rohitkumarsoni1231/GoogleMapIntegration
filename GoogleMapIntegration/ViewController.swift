0fbdb3ba3075358cc0d4077706a5afda77293164
Rohit Kumar <oasisrohit470@gmail.com>
2023-11-13 16:51:35 +0400

    Google Map Integration in IOS Project

--- GoogleMapIntegration/ViewController.swift
+++ GoogleMapIntegration/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationServices()
+    }
+    
+    func checkLocationServices() {
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                self.setupLocationManager()
+                self.checkLocationAuthorization()
+                self.startTrackingUserLocation()
+            } else {
+                // show Alert Letting the user know they have to turn location on.
+            }
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways, .denied:
+            break
+        case .notDetermined , .restricted:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func startTrackingUserLocation() {
+        mapView.isMyLocationEnabled = true
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func didTapOnCurrentLocation(_ sender: Any) {
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization() // or requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 8, bearing: 0, viewingAngle: 0)
+
+            let marker = GMSMarker()
+            marker.position = location.coordinate
+            marker.title = "Hello Rohit"
+            marker.snippet = "I'm here"
+            marker.map = mapView
+        }
+    }
 
 }
-
